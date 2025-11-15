47fb5383c532767e1499da5ec853084a44e073ca
Reece <reecesucks@gmail.com>
2025-11-15 16:34:35 +1100

    Rotary encoder fix

--- WiimController/Classes/RotaryEncoder.cs
+++ WiimController/Classes/RotaryEncoder.cs
@@ -9,12 +9,13 @@ namespace WiimController.Classes
         private readonly int _pinA;
         private readonly int _pinB;
         private readonly int _pinButton;
-
         private readonly WiimService _wiimService;
         private readonly GpioController _controller;
-        public int Count { get; private set; }
-
-        public event Action<int>? Rotated;       // +1 / -1
+        private readonly Timer _rotationTimer;
+        private const int RotationDelayMs = 25;
+        private int _rotationbuffer;
+        private int _count = 0;
+        public event Action<int>? RotatedAction;       // +1 / -1
         public event Action? ButtonPressed;
 
         public RotaryEncoder(int pinA, int pinB, int pinButton, WiimService wiimService, GpioController? controller = null)
@@ -37,6 +38,14 @@ namespace WiimController.Classes
                 PinEventTypes.Rising,
                 OnEncoderARising
             );
+
+            _controller.RegisterCallbackForPinValueChangedEvent(
+                _pinButton,
+                PinEventTypes.Rising,
+                CheckButton
+            );
+
+            _rotationTimer = new Timer(OnRotationTimerElaped, null, Timeout.Infinite, Timeout.Infinite);
         }
 
         private void OnEncoderARising(object sender, PinValueChangedEventArgs args)
@@ -46,18 +55,29 @@ namespace WiimController.Classes
 
             int direction = (a == b) ? +1 : -1;
 
-            Count += direction;
-            Rotate(Count);
-         //   Rotated?.Invoke(direction);
+            _rotationbuffer +=direction;
+            _count += direction;
+
+            _rotationTimer.Change(RotationDelayMs, Timeout.Infinite);
+
         }
 
-        private async void Rotate(int direction)
+        private void OnRotationTimerElaped(object? state)
         {
-            _wiimService.Volume += direction;
+            if (_rotationbuffer != 0)
+            {
+                Rotated(_rotationbuffer);
+                _rotationbuffer = 0;
+            }
+        }
+
+        private async void Rotated(int rotationValue)
+        {
+            _wiimService.Volume += rotationValue;
             await _wiimService.SetVolume(_wiimService.Volume);
         }
 
-        public async void CheckButton()
+        public async void CheckButton(object sender, PinValueChangedEventArgs args)
         {
             if (_controller.Read(_pinButton) == PinValue.Low)
             {
