12d86b3895dcca78d45c134fec65bd507a9da65c
Reece Shandro <reeceshandro@outlook.com>
2025-11-15 15:38:20 +1100

    rotary knob conntroller

--- WiimController/Program.cs
+++ WiimController/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Device.Gpio;
 using Microsoft.Extensions.Configuration;
+using WiimController.Classes;
 using WiimController.Services;
 
 
@@ -96,7 +97,6 @@ class Program
     {
         using var controller = new GpioController();
 
-        
         int btnNext3Pin = 26;
         int btnPrevious4Pin = 23;
 
@@ -104,56 +104,18 @@ class Program
         controller.OpenPin(btnPrevious4Pin, PinMode.InputPullUp);
 
         //ToDo Rotary Knob for volume
-        int s1Pin = 17;   // GPIO pin connected to S1
-        int s2Pin = 27;
-        int keyPin = 27;
-        
-        int lastS1 = 0;    
-        int lastS2 = 0;
-        int position = 0;
-
-        controller.OpenPin(s1Pin, PinMode.InputPullUp);
-        controller.OpenPin(s2Pin, PinMode.InputPullUp);
-        controller.OpenPin(keyPin, PinMode.InputPullUp);
-
-        Console.WriteLine("Listening for GPIO button presses (Ctrl+C to exit)...");
-
-        lastS1 = (int)controller.Read(s1Pin);
-        lastS2 = (int)controller.Read(s2Pin);
+        var encoder = new RotaryEncoder(
+            wiimService: wiimService,
+            pinA: 17,
+            pinB: 22,
+            pinButton: 27,
+            controller: controller
+        );
 
         while (true)
         {
-            int s1 = (int)controller.Read(s1Pin);
-            int s2 = (int)controller.Read(s2Pin);
-
-            // Detect rotation
-            if (s1 != lastS1 || s2 != lastS2)
-            {
-                if (lastS1 == s2 && lastS2 != s1)
-                {
-                    position++;  // Clockwise
-                    Console.WriteLine($"Rotated CW. Position: {position}");
-                    wiimService.SetVolume(position);
-                }
-                else if (lastS2 == s1 && lastS1 != s2)
-                {
-                    position--;  // Counter-clockwise
-                    Console.WriteLine($"Rotated CCW. Position: {position}");
-                    wiimService.SetVolume(position);
-                }
-
-                lastS1 = s1;
-                lastS2 = s2;
-            }
 
             // Detect button press
-            if (controller.Read(keyPin) == 0)  // Active-low button
-            {
-                Console.WriteLine("Button pressed!");
-                Thread.Sleep(200);  // Debounce delay
-            }
-
-
 
             if (controller.Read(btnNext3Pin) == PinValue.Low)
             {
