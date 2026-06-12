c8c0699e202d3a57956c6904c2463abb3e42569a
Reece Shandro <reeceshandro@outlook.com>
2026-06-12 19:13:10 +1000

    Blank out values in secrets.example.h template

--- WiimController/WiimComs-ESP32/include/secrets.example.h
+++ WiimController/WiimComs-ESP32/include/secrets.example.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define WIFI_SSID      "wifi name"
-#define WIFI_PASSWORD  "wifi password"
-#define WIIM_BASE_URL  "https://(IP ADDRRESS)"
+#define WIFI_SSID      ""
+#define WIFI_PASSWORD  ""
+#define WIIM_BASE_URL  ""
